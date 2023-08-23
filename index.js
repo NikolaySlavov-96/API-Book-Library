4feefcefaf363de07c20ae37f39c47faf5da71db
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-23 18:39:25 +0300

    I created small API with using PostgreSQL DB

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const db = require('./config/database');
+
+start();
+
+async function start() {
+    const app = express();
+
+    app.use(express.json());
+
+    app.get('/books',db.getDate);
+    app.get('/books/:id', db.getDateById);
+    app.post('/books', db.createBook);
+    app.put('/books/:id', db.updateBook);
+    app.delete('/books/:id', db.deleteBook);
+
+    app.listen(3000, () => console.log('Application works'))
+}
