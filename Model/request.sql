33f83907e801cd34785ad308cc60e1a8b8397e9b
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-08-27 23:06:33 +0300

    adding more requests

--- Model/request.sql
+++ Model/request.sql
@@ -56,3 +56,33 @@ SELECT
 FROM account AS u
     JOIN user_book AS ub ON ub.user_id = u.id
     JOIN book AS b ON ub.book_id = b.id;
+
+DROP TABLE user_book;
+
+CREATE TABLE
+    user_book (
+        user_id INT NOT NULL,
+        book_id INT NOT NULL,
+        CONSTRAINT pk_user_book PRIMARY KEY (user_id, book_id),
+        CONSTRAINT fk_user_book FOREIGN KEY (user_id) REFERENCES account (id),
+        CONSTRAINT fk_user_book_book FOREIGN KEY (book_id) REFERENCES book (id)
+    );
+
+SELECT
+    u.email,
+    b.author,
+    b.booktitle
+FROM account AS u
+    JOIN user_book AS ub ON ub.user_id = u.id
+    JOIN book AS b ON ub.book_id = b.id
+WHERE
+    b.author LIKE 'Остин Клиън';
+
+SELECT
+    u.email,
+    b.author,
+    b.booktitle
+FROM account AS u
+    JOIN user_book AS ub ON ub.user_id = u.id
+    JOIN book AS b ON ub.book_id = b.id
+LIMIT 5;
