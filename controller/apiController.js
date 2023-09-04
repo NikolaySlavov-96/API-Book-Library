895d06f9f9f248ece571245ff4264a262cc76fee
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-09-04 21:08:01 +0300

    adding new requestion and enhanced modul

--- controller/apiController.js
+++ controller/apiController.js
@@ -3,8 +3,18 @@ const apiService = require('../services/apiService');
 const querys = {
     'Select': {
         'book': ({ offset, limit }) => `SELECT (SELECT COUNT(*) FROM book) as count, (SELECT json_agg(t.*) FROM(SELECT * FROM book OFFSET ${offset} LIMIT ${limit}) AS t) AS rows`,
-        'universalbook': ({ userId, limit, offset, type }) => `SELECT (SELECT COUNT(*) FROM user_book_${type}) as count, (SELECT json_agg(t.*) FROM(SELECT u.email, b.author, b.booktitle FROM account AS u JOIN user_book_${type} AS ubp ON ubp.user_id = u.id JOIN book AS b ON ubp.book_id = b.id WHERE u.id = ${userId} OFFSET ${offset} LIMIT ${limit}) AS t) AS rows`,
-        'search': ({ offset, limit, search }) => `SELECT (SELECT COUNT(*) FROM book WHERE booktitle LIKE '%${search}%' OR author LIKE '%${search}%' OR genre LIKE '%${search}%') as count, (SELECT json_agg(t.*) FROM(SELECT * FROM book WHERE booktitle LIKE '%${search}%' OR author LIKE '%${search}%' OR genre LIKE '%${search}%' OFFSET ${offset} LIMIT ${limit}) AS t) AS rows`
+        'universalbook': ({ userId, limit, offset, type }) => `SELECT (SELECT COUNT(*) FROM user_book_${type}) as count, (SELECT json_agg(t.*) FROM(SELECT u.email, b.author, b.booktitle FROM account AS u JOIN user_book_${type} AS ubp ON ubp.user_id = u.id JOIN book AS b ON ubp.book_id = b.id WHERE u.id = ${userId} AND ubp.isdelete = false OFFSET ${offset} LIMIT ${limit}) AS t) AS rows`,
+        'search': ({ offset, limit, search }) => `SELECT (SELECT COUNT(*) FROM book WHERE booktitle LIKE '%${search}%' OR author LIKE '%${search}%' OR genre LIKE '%${search}%') as count, (SELECT json_agg(t.*) FROM(SELECT * FROM book WHERE booktitle LIKE '%${search}%' OR author LIKE '%${search}%' OR genre LIKE '%${search}%' OFFSET ${offset} LIMIT ${limit}) AS t) AS rows`,
+        'bookid': ({ id }) => `SELECT * FROM book WHERE id = ${id}`,
+        'forpurchase': ({ user_id, book_id }) => `SELECT * FROM user_book_forpurchase WHERE user_id = ${user_id} AND book_id = ${book_id}`,
+    },
+    'Insert': {
+        'book': () => `INSERT INTO book (author, booktitle) VALUES ($1, $2) RETURNING *`,
+        'forpurchase': () => `INSERT INTO user_book_forpurchase (user_id, book_id) VALUES ($1, $2) RETURNING *`
+    },
+    'Update': {
+        'book': ({ author, booktitle, id }) => `UPDATE book SET author = ${author}, booktitle = ${booktitle} WHERE id = ${id} RETURNING *`,
+        'forpurchase': ({ user_id, book_id, condition }) => `UPDATE user_book_forpurchase SET isdelete = ${!condition} WHERE user_id = ${user_id} AND book_id = ${book_id} RETURNING *`,
     }
 }
 
@@ -30,7 +40,7 @@ const getAllDate = async (req, res) => {
     };
 
     try {
-        const result = await apiService.getAllDate(querys['Select'][typesQuery](values), res.body);
+        const result = await apiService.getAllDate(querys['Select'][typesQuery](values));
         const rows = result.rows[0];
 
         res.status(200).json({ rows: rows.rows, count: rows.count });
@@ -41,10 +51,10 @@ const getAllDate = async (req, res) => {
 
 const getDateById = async (req, res) => {
     const id = parseInt(req.params.id);
-    const query = 'SELECT * FROM book WHERE id = $1';
+    // const query = 'SELECT * FROM book WHERE id = $1';
 
     try {
-        const result = await apiService.getDateById(query, id);
+        const result = await apiService.getDateById(querys['Select']['bookid']({ id }));
         res.status(200).json(result.rows);
     } catch (err) {
 
@@ -52,23 +62,37 @@ const getDateById = async (req, res) => {
 }
 
 const createBook = async (req, res) => {
-    const query = 'INSERT INTO book (author, booktitle) VALUES ($1, $2) RETURNING *';
+    const type = req.params.type;
+    const user_id = req.user.id;
+    const { author, booktitle, book_id } = req.body;
+
+    const array = {
+        'book': [author, booktitle],
+    }
 
     try {
-        const result = await apiService.create(query, req.body);
+        const verify = type !== 'book' && await apiService.getDateById(querys['Select'][type]({ user_id, book_id }));
+        if (verify?.rows?.length) {
+            const condition = verify.rows[0].isdelete;
+            const resulst = await apiService.update(querys['Update'][type]({ user_id, book_id, condition }));
+            res.status(201).json(resulst.rows[0]);
+        }
+
+        const result = await apiService.create(querys['Insert'][type](), array[type]);
         res.status(201).json(result.rows[0]);
     } catch (err) {
-
+        console.log(err);
     }
 }
 
 const updateBook = async (req, res) => {
+    // const query = 'UPDATE book SET author = $1, booktitle = $2 WHERE id = $3';
     const id = parseInt(req.params.id);
-    const query = 'UPDATE book SET author = $1, booktitle = $2 WHERE id = $3';
-
+    const type = req.params.type;
+    const { author, booktitle } = req.body;
     try {
-        const result = await apiService.update(query, id, req.body);
-        res.status(200).send(`Book modified with ID: ${id}`)
+        const result = await apiService.update(querys['Update'][type]({ author, booktitle, id }));
+        res.status(200).send(result.rows[0]);
     } catch (err) {
 
     }
