7f1689bb27a0653e55305a7b8a3f7571e763cecd
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-09-06 18:01:59 +0300

    using new path

--- config/router.js
+++ config/router.js
@@ -1,7 +1,9 @@
 const auth = require('../routes/auth');
 const book = require('../routes/book');
+const withUser = require('../routes/withUser');
 
 module.exports = (app) => {
     app.use('/auth', auth);
     app.use('/book', book);
+    app.use('/users', withUser);
 }
