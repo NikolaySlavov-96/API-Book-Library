f9511de53de774a0b210a12424a8feb80c48f22c
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-10-02 21:07:04 +0300

    using dotenv and adding and https

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
+const https = require('https');
+const fs = require('fs');
+require('dotenv').config();
+
 const express = require('express');
 
 const useExpress = require('./config/useExpress');
 const router = require('./config/router');
+const PORT = process.env.PORT;
+
+/*
+const options = {
+    key: fs.readFileSync('../key-PRK.pem'),
+    cert: fs.readFileSync('../cert-CRT.pem')
+}
+*/
+
 
 start();
 
 async function start() {
     const app = express();
 
+    // https.createServer(options, app).listen(PORT, () => console.log('Server work on port ' + PORT));
+
     useExpress(app, express);
     router(app);
 
-    app.listen(3030, () => console.log('Application works'))
+    app.listen(PORT, () => console.log('Application works'))
 }
