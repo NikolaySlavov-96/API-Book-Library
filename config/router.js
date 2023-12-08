640306efef1183b3a9e70af54cfad06690afd055
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-12-08 20:07:52 +0200

    clear all branch

--- config/router.js
+++
@@ -1,9 +0,0 @@
-const auth = require('../routes/auth');
-const book = require('../routes/book');
-const withUser = require('../routes/withUser');
-
-module.exports = (app) => {
-    app.use('/auth', auth);
-    app.use('/book', book);
-    app.use('/users', withUser);
-}
