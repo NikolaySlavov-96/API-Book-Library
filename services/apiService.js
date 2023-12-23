ef03bb70bab12fc8248bf47dfc066a507e5d8b4c
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2023-12-23 13:04:13 +0200

    adding sequelize library in service module and change logic

--- services/apiService.js
+++ services/apiService.js
@@ -1,39 +1,48 @@
-const { Book } = require('../Model/BookModel');
+const { Book, Op } = require('../Model/BookModel');
 
-const getAllDate = async (query) => {
-    const result = await Book.findAll();
-    return result;
+const getAllDate = async (query, type, search) => {
+
+    const typeOf = {
+        'Op.like': Op.like,
+    }
+
+    type && (query.where = {
+        [Op.or]: [{ booktitle: { [typeOf[type]]: search } }, { author: { [typeOf[type]]: search } }, { genre: { [typeOf[type]]: search } }]
+    })
+    // return Book.findAll({});
+    return Book.findAndCountAll(query);
 }
 
 const getDateById = async (id) => {
-    const result = await Book.findOne({ where: { id: id } })
-    return result;
+    return Book.findByPk(id)
 }
 
-const create = async (query, array) => {
+const create = async (query) => {
     /*
     const isBook = await Book.findOne({ where: { email: email } });
 
     if (isBook) {
         throw new Error('Book is created befor');
     }
-    const createEmail = await Book.create({ book, release_date: "2023-12-14", });
+    */
+    const createEmail = await Book.create(query);
 
     return createEmail;
-    */
-    const result = await dbConnect.query(query, array);
-    return result;
 }
 
-const update = async (query) => {
-    const result = await dbConnect.query(query);
+const update = async ({ author, booktitle, id }) => {
+    const data = await Book.findByPk(id)
+
+    data.author = author;
+    data.booktitle = booktitle;
+    const result = await data.save();
     return result
 }
 
 
-const remove = async (query, id) => {
-    const result = await dbConnect.query(query, [id]);
-    return result;
+const remove = async (id) => {
+    const data = await Book.findByPk(id);
+    return data.destroy();
 }
 
 
