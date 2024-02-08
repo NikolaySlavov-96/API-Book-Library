ac975ca7c7a9403fee933874f8c1823a7fe6b553
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-02-08 15:04:49 +0200

    adding of global error resposne

--- config/router.js
+++ config/router.js
@@ -6,4 +6,14 @@ module.exports = (app) => {
     app.use('/auth', auth);
     app.use('/book', book);
     app.use('/users', withUser);
+
+    app.use((error, req, res, next) => {
+        console.log("Global Error - ", error)
+    
+        res.status(error.status || 500);
+        res.json({
+          userInfo: {},
+          message: error.status ? error : "Something whent wrong!"
+        });
+      });
 }
