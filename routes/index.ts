c74c1e3a253ac72a877082500d3bce70873003e2
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-08-24 14:02:04 +0300

    refactor: Remove withUser from routes

--- routes/index.ts
+++ routes/index.ts
@@ -2,4 +2,3 @@ export { default as auth, } from './_authRoutes';
 export { default as book, } from './_bookRoutes';
 export { default as bookState } from './_bookStateRoutes';
 export { default as search, } from './_searchRoutes';
-export { default as withUser, } from './_withUserRoutes';
