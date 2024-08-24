94305f6e193d6be0db6f40c9154d7d7e1d1b009e
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-08-24 12:09:23 +0300

    refactor: Improve Auth Route

--- routes/_authRoutes.ts
+++ routes/_authRoutes.ts
@@ -6,30 +6,30 @@ const auth = Router();
 import { expressValidator, } from '../middleware';
 
 import * as authController from '../controller/authController';
+import { PASSWORD_PATTERN, ROUTING_MESSAGES, } from '../constants';
 
 
 auth.post('/login',
-    body('email').isEmail().withMessage('Email is requied'),
-    body('password').notEmpty().withMessage('Passwor is required'),
+    body('email').isEmail().withMessage(ROUTING_MESSAGES.EMAIL_IS_REQUIRED),
+    body('password').notEmpty().withMessage(ROUTING_MESSAGES.PASSWORD_IS_REQUIRED),
     expressValidator,
     authController.getUser
 );
+
 auth.post('/register',
-    body('email').isEmail().withMessage('Emais is not corret'),
-    body('password').isLength({ min: 8, }).withMessage('Invalid password').bail()
-        .matches(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ]+$/).withMessage('Incorrect type of password'),
-    body('rePassword').custom((value, { req, }) => {
-        return value == req.body.password;
-    }).withMessage('Password missmatch!'),
-    body('year').notEmpty().withMessage('Years is requited'),
+    body('email').isEmail().withMessage(ROUTING_MESSAGES.EMAIL_ADDRESS_INCORRECT),
+    body('password').isLength({ min: 8, }).withMessage(ROUTING_MESSAGES.INVALID_PASSWORD).bail()
+        .matches(PASSWORD_PATTERN).withMessage(ROUTING_MESSAGES.INCORRECT_TYPE_PASSWORD),
+    body('year').notEmpty().withMessage(ROUTING_MESSAGES.YEARS_IS_REQUIRED),
     expressValidator,
     authController.createUser
 );
+
 auth.post('/logout', authController.exitUset);
 auth.get('/check', authController.checkFields);
 
 auth.post('/verify',
-    body('verifyToken').notEmpty().withMessage('Verify Token is required'),
+    body('verifyToken').notEmpty().withMessage(ROUTING_MESSAGES.TOKE_IS_REQUIRED),
     expressValidator,
     authController.verifyUser
 );
