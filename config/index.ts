a76b10504cc0ad9a2856692ef3f642d9c10772c9
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-06 21:16:27 +0300

    feat: Install and initialize Socket.IO

--- config/index.ts
+++ config/index.ts
@@ -1,4 +1,5 @@
 export { _checkDatabaseIfItExist as checkDatabaseIfItExist, } from './_database';
+
 export { default as emailConfig, } from './_emailConfig';
 export { default as expressConfig, } from './_express';
 export { default as initNewConnection, } from './_database';
