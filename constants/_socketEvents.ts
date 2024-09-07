473174e69190078bd9ee4c5a8beb8afa742fb1a0
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-07 18:30:42 +0300

    feat: Add constants for Socket.IO events

---
+++ constants/_socketEvents.ts
@@ -0,0 +1,5 @@
+export enum _EReceiveEvents { }
+
+export enum _ESendEvents {
+    NEW_BOOK_ADDED = 'new-book-added',
+}
--- constants/index.ts
+++ constants/index.ts
@@ -1,6 +1,9 @@
 export { PASSWORD_PATTERN, } from './_routingMessage';
 export { default as EMAIL, } from './_emailConstants';
 
+export { _ESendEvents as ESendEvents, } from './_socketEvents';
+export { _EReceiveEvents as EReceiveEvents, } from './_socketEvents';
+
 export { _tableName as TABLE_NAME, } from './_dataBaseNames';
 export { default as MESSAGES, } from './_messages';
 export { default as ROUTING_MESSAGES, } from './_routingMessage';
