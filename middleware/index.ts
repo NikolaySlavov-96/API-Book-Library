78e0b9629e08dd46f6dd5f666638a84d8d4f2b90
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-09-08 08:55:00 +0300

    chore: Remove unused code for cleaner codebase

--- middleware/index.ts
+++ middleware/index.ts
@@ -5,6 +5,4 @@ export { default as socketMiddleware, } from './_socketMiddleware';
 export { default as trimBody, } from './_trimBody';
 
 export { default as auth, } from './authentication/_auth';
-export { default as hasPermission, } from './authentication/_hasPermission';
 export { default as isAuthenticated, } from './authentication/_isAuthenticated';
-export { default as isExistingAndBonus, } from './authentication/_isExistingAndBonus';
