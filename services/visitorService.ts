ce178f26a764b4fecfa09e5c403331f90009bf5a
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-04 20:50:12 +0300

    feat: Implement visitorService to create records for today's unique visitors in Redis and store unique visitor records in MongoDB

---
+++ services/visitorService.ts
@@ -0,0 +1,48 @@
+import { getCurrentDate, calculateRelativeDate, } from '../Helpers';
+
+import { redisClient, } from '../config';
+
+import UserDataModel from '../Model/UserDataModel';
+
+export const storeVisitorInfo = async (data) => {
+    const redisKey = getCurrentDate();
+    const userIp = data.IPv4;
+
+    const returnedData = {
+        dailyUsers: 0,
+        uncialUsers: 0,
+        isNewUser: false,
+    };
+
+    // Check and Inset and Mongo DB
+    const resultMongo = await UserDataModel.findOne({ userAddress: userIp, });
+    if (!resultMongo) {
+        await UserDataModel.create({ userAddress: userIp, });
+    }
+    const allUncialUser = await UserDataModel.countDocuments();
+    returnedData.uncialUsers = allUncialUser;
+
+    // Check and Insert in Redis
+    // Added record in Array
+    const resultRedis = await redisClient.sAdd(redisKey, userIp);
+    if (resultRedis) {
+        returnedData.isNewUser = true;
+    }
+    // Create new records
+    // const uniqueIPs = await redisClient.sMembers('key');
+
+    // Return only count of exist records in Array
+    const uniqueIPs = await redisClient.sCard(redisKey);
+    returnedData.dailyUsers = uniqueIPs;
+
+    return returnedData;
+};
+
+const deleteKey = async () => {
+    const key = calculateRelativeDate(1, 'day');
+    try {
+        await redisClient.del(key);
+    } catch (err) {
+        console.log(err);
+    }
+};
