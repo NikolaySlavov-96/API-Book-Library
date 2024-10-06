3b357b564af8231d718a49f124d5b8ae07286ed0
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-06 13:37:06 +0300

    added temporary events

--- constants/_socketEvents.ts
+++ constants/_socketEvents.ts
@@ -1,6 +1,7 @@
 export enum _EReceiveEvents {
     USER_JOINED = 'user_joined',
     SUPPORT_CHAT_USER_JOIN = 'support_chat_join',
+    SUPPORT_ACCEPT_USER = 'support_accept_user', // not used
     RECEIVE_SUPPORT_MESSAGE = 'receive_support_message',
 }
 
@@ -8,6 +9,7 @@ export enum _ESendEvents {
     NEW_BOOK_ADDED = 'new-book-added',
     USER_JOINED = 'user_joined',
     SUPPORT_CHAT_USER_JOIN_ACKNOWLEDGMENT = 'support_chat_user_join_acknowledgment',
+    SUPPORT_ACCEPT_USER_ACKNOWLEDGMENT = 'support_accept_user_acknowledgment', // not used
     SEND_SUPPORT_MESSAGE = 'send_support_message',
     NOTIFY_ADMINS_OF_NEW_USER = 'notify_admins_of_new_user',
 }
