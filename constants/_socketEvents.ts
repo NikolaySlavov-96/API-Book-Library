ad878ae96b3e189b72cf5379e839613171b4e30d
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-09 20:18:46 +0300

    feat: Add new events

--- constants/_socketEvents.ts
+++ constants/_socketEvents.ts
@@ -1,7 +1,8 @@
 export enum _EReceiveEvents {
     USER_JOINED = 'user_joined',
     SUPPORT_CHAT_USER_JOIN = 'support_chat_join',
-    SUPPORT_ACCEPT_USER = 'support_accept_user', // not used
+    SUPPORT_CHAT_USER_LEAVE = 'support_chat_leave',
+    SUPPORT_ACCEPT_USER = 'support_accept_user',
     RECEIVE_SUPPORT_MESSAGE = 'receive_support_message',
 }
 
@@ -9,7 +10,7 @@ export enum _ESendEvents {
     NEW_BOOK_ADDED = 'new-book-added',
     USER_JOINED = 'user_joined',
     SUPPORT_CHAT_USER_JOIN_ACKNOWLEDGMENT = 'support_chat_user_join_acknowledgment',
-    SUPPORT_ACCEPT_USER_ACKNOWLEDGMENT = 'support_accept_user_acknowledgment', // not used
+    SUPPORT_ACCEPT_USER_ACKNOWLEDGMENT = 'support_accept_user_acknowledgment',
     SEND_SUPPORT_MESSAGE = 'send_support_message',
     NOTIFY_ADMINS_OF_NEW_USER = 'notify_admins_of_new_user',
 }
