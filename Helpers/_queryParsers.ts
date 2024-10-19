302a4292a34416ba4c54c92671f030ceb09cf3cc
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-19 13:42:20 +0300

    refactor: Remove unused constants in emailParser

--- Helpers/_queryParsers.ts
+++ Helpers/_queryParsers.ts
@@ -12,20 +12,10 @@ export const _pageParser = (query) => {
 };
 
 export const _emailParser = (query) => {
-    const page = parseInt(query?.page) || 1;
-    const limit = parseInt(query?.limit) || 10;
-
-    const maxLimit = Math.min(limit, 140);
-    const skipSource = (page - 1) * maxLimit;
-
     const email = query?.email as string;
-    const searchContent = query.search && `%${query?.search}%`;
 
     return {
-        limit: maxLimit,
-        offset: skipSource,
         email,
-        searchContent,
     };
 };
 
