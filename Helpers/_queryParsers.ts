be40dd4866493e038e8e9bf08e56027ba3942b43
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-19 13:37:59 +0300

    refactor: Rename query parser to query parsers and add separate logic for each case

---
+++ Helpers/_queryParsers.ts
@@ -0,0 +1,57 @@
+const _queryParser = (query) => {
+    const page = parseInt(query?.page) || 1;
+    const limit = parseInt(query?.limit) || 10;
+
+    const maxLimit = Math.min(limit, 140);
+    const skipSource = (page - 1) * maxLimit;
+
+    const email = query?.email as string;
+    const searchContent = query.search && `%${query?.search}%`;
+
+    return {
+        limit: maxLimit,
+        offset: skipSource,
+        email,
+        searchContent,
+    };
+};
+export default _queryParser;
+
+export const _pageParser = (query) => {
+    const page = parseInt(query?.page) || 1;
+    const limit = parseInt(query?.limit) || 10;
+
+    const maxLimit = Math.min(limit, 140);
+    const skipSource = (page - 1) * maxLimit;
+
+    return {
+        limit: maxLimit,
+        offset: skipSource,
+    };
+};
+
+export const _emailParser = (query) => {
+    const page = parseInt(query?.page) || 1;
+    const limit = parseInt(query?.limit) || 10;
+
+    const maxLimit = Math.min(limit, 140);
+    const skipSource = (page - 1) * maxLimit;
+
+    const email = query?.email as string;
+    const searchContent = query.search && `%${query?.search}%`;
+
+    return {
+        limit: maxLimit,
+        offset: skipSource,
+        email,
+        searchContent,
+    };
+};
+
+export const _searchParser = (query) => {
+    const searchContent = query.search && `%${query?.search}%`;
+
+    return {
+        searchContent,
+    };
+};
