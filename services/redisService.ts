51c529ad4d107c2e6da4a87b8e9455c9df9e4780
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-20 12:11:42 +0300

    feat: Implement functionality to find keys by partial match and clear all matching keys

--- services/redisService.ts
+++ services/redisService.ts
@@ -12,16 +12,6 @@ export const cacheDataWithExpiration = async (key, data, time = redisCacheTimes.
     await redisClient.setEx(key, time, JSON.stringify(data));
 };
 
-export const deleteCacheEntry = async (key) => {
-    const result = await redisClient.del(key);
-
-    if (result === 1) {
-        return true;
-    }
-
-    return false;
-};
-
 // Use Redis SET to store only unique data entries,
 // automatically handling duplicates
 export const fetchSetMembers = async (key) => {
@@ -35,3 +25,38 @@ export const fetchSetSize = async (key) => {
 export const addDataToSet = async (key, data) => {
     return await redisClient.sAdd(key, data);
 };
+
+// Implement functionality to delete specific data or keys from Redis
+export const deleteCacheEntry = async (key) => {
+    const result = await redisClient.del(key);
+
+    if (result === 1) {
+        return true;
+    }
+
+    return false;
+};
+export const deleteKeysWithPrefix = async (prefix) => {
+    try {
+        let cursorCount = 0;
+        do {
+            const { cursor, keys, } = await redisClient.scan(cursorCount, {
+                MATCH: `${prefix}*`,
+                COUNT: 20,
+            });
+
+            cursorCount = cursor;
+
+            if (keys.length > 0) {
+                await deleteCacheEntry(keys);
+            }
+        } while (cursorCount !== 0);
+    } catch (err) {
+        console.error('Error ~ deleteKeysWithPrefix: ', err);
+    } finally {
+        // If a new Redis connection is created solely for erasing keys, ensure to 
+        // call "quit" to properly close the connection afterward
+        // redisClient.quit();
+    }
+};
+
