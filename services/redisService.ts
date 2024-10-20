ee607e8289b7a5b7c47e6a4e7272296dd953e57f
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-20 10:24:18 +0300

    feat: Enhance Redis Service with additional functionalities: delete by key, fetch members and size, and add to set

--- services/redisService.ts
+++ services/redisService.ts
@@ -2,10 +2,36 @@ import { redisClient, } from '../config';
 
 import { redisCacheTimes, } from '../constants';
 
+// Store and manage cached data using Redis for
+// improved performance and quick data retrieval
 export const fetchCacheData = async (key) => {
     return await redisClient.get(key);
 };
 
 export const cacheDataWithExpiration = async (key, data, time = redisCacheTimes.HOURS) => {
     await redisClient.setEx(key, time, JSON.stringify(data));
+};
+
+export const deleteCacheEntry = async (key) => {
+    const result = await redisClient.del(key);
+
+    if (result === 1) {
+        return true;
+    }
+
+    return false;
+};
+
+// Use Redis SET to store only unique data entries,
+// automatically handling duplicates
+export const fetchSetMembers = async (key) => {
+    return await redisClient.sMembers(key);
+};
+
+export const fetchSetSize = async (key) => {
+    return await redisClient.sCard(key);
+};
+
+export const addDataToSet = async (key, data) => {
+    return await redisClient.sAdd(key, data);
 };
