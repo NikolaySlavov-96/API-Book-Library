4d9c5d57c28553fcd3ba1447983304565bd39d30
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-10-26 11:00:11 +0300

    added and increment minor version

--- bash/update_build_number.sh
+++ bash/update_build_number.sh
@@ -14,11 +14,32 @@ increment_patch_version() {
     echo "${version_parts[0]}${delimiter}${version_parts[1]}${delimiter}${version_parts[2]}"
 }
 
+increment_minor_version() {
+    local version=$1
+    local delimiter=.
+
+    # Split version into array
+    local version_parts=(${version//./ })
+
+    # Increment the minor version and reset patch version
+    version_parts[1]=$((version_parts[1] + 1))
+    version_parts[2]=0
+
+    # Join the array back into a string
+    echo "${version_parts[0]}${delimiter}${version_parts[1]}${delimiter}${version_parts[2]}"
+}
+
 version=$(jq -r '.version' package.json)
 build_number=$(jq -r '.buildNumber // 0' package.json)
 
-# Increment the patch version and build number
-new_version=$(increment_patch_version "$version")
+if [[ "$GITHUB_EVENT_NAME" == "pull_request" && "$(jq -r '.action' "$GITHUB_EVENT_PATH")" == "closed" && "$(jq -r '.pull_request.merged' "$GITHUB_EVENT_PATH")" == "true" ]]; then
+    # Increment minor version for merged pull requests
+    new_version=$(increment_minor_version "$version")
+else
+    # Increment patch version for direct pushes
+    new_version=$(increment_patch_version "$version")
+fi
+
 new_build_number=$((build_number + 1))
 
 # Update package.json with the new version and build number
