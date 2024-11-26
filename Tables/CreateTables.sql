6bccb8e8ec9fe50bd6fae80fff79527d86f0db6f
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-26 18:53:26 +0200

    feat: Enhance table creation with IF NOT EXISTS checks

--- Tables/CreateTables.sql
+++ Tables/CreateTables.sql
@@ -1,18 +1,17 @@
-CREATE table products (
+CREATE table IF NOT EXISTS products (
     ID SERIAL PRIMARY KEY,
     authorId INT not NULL,
     productTitle VARCHAR(140) UNIQUE,
     genre VARCHAR(45),
     isVerify BOOLEAN default false
 );
-   
-create table authors (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(60),
-	image VARCHAR(145),
-	genre VARCHAR(45),
-	isVerify BOOLEAN default false 
-	authorId INT REFERENCES products(id)
+
+create table IF NOT EXISTS authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(60),
+    image VARCHAR(145),
+    genre VARCHAR(45),
+    isVerify BOOLEAN default false authorId INT REFERENCES products (id)
 );
 
 CREATE TEMPORARY TABLE bookAuthorPairs (
