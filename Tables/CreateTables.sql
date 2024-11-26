bcd0ddc619975babbba9b81229acf90e2ab434e4
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-26 19:09:46 +0200

    feat: Implement loop to add only unique products and authors

--- Tables/CreateTables.sql
+++ Tables/CreateTables.sql
@@ -19,3 +19,28 @@ CREATE TEMPORARY TABLE bookAuthorPairs (
     productTitle VARCHAR(140),
     authorName VARCHAR(60)
 );
+
+DO $$
+DECLARE
+    v_productTitle VARCHAR(255);
+    v_authorName VARCHAR(255);
+    authorId INT;
+	productId INT;
+BEGIN
+    FOR v_productTitle, v_authorName IN
+        SELECT productTitle, authorName FROM bookAuthorPairs
+    LOOP
+        SELECT id INTO authorId FROM authors WHERE name = v_authorName LIMIT 1;
+        IF authorId IS NULL THEN
+            INSERT INTO authors (name) VALUES (v_authorName) RETURNING id INTO authorId;
+        END IF;
+
+		SELECT id INTO productId FROM products WHERE productTitle = v_productTitle LIMIT 1;
+		IF productId IS NULL THEN
+			INSERT INTO products (productTitle, authorId) VALUES (v_productTitle, authorId);
+		END IF;
+    END LOOP;
+
+	DROP TABLE IF EXISTS bookAuthorPairs;
+
+END $$;
