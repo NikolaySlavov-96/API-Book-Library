ff82c23c582cd067eae700baecf18f5badbbefa3
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-27 20:46:49 +0200

    chore: Remove deprecated code from the codebase

--- config/_router.ts
+++ config/_router.ts
@@ -1,10 +1,9 @@
-import { search, auth, book, product, } from '../routes';
+import { search, auth, product, } from '../routes';
 
 const PREFIX = '';
 
 export default (app) => {
     app.use(`${PREFIX}/auth`, auth);
-    app.use(`${PREFIX}/book`, book); // DEPRECATED
     app.use(`${PREFIX}/product`, product);
     app.use(`${PREFIX}/search`, search);
 };
--- routes/index.ts
+++ routes/index.ts
@@ -1,4 +1,3 @@
 export { default as auth, } from './_authRoutes';
-export { default as book, } from './_bookRoutes';
 export { default as product, } from './_productRoutes';
 export { default as search, } from './_searchRoutes';
