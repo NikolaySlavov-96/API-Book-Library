39f85fec81565977d6209986fbb902d9fac122d2
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-27 20:37:58 +0200

    feat: Implement and add ProductStatusService and ProductStatusController

---
+++ controller/productStatusController.ts
@@ -0,0 +1,79 @@
+import { MESSAGES, queryOperators, cacheKeys, RESPONSE_STATUS_CODE, } from '../constants';
+
+import { buildCacheKey, pageParser, searchParser, } from '../Helpers';
+
+import * as productStatusService from '../services/productStatusService';
+import {
+    getUserVerificationStatus,
+} from '../services/getUserVerificationStatus';
+import { cacheDataWithExpiration, deleteCacheEntry, } from '../services/cacheService';
+import { getAllStates, } from '../services/stateService';
+
+import { updateMessage, } from '../util';
+
+
+export const getAllStatus = async (req, res, next) => {
+    try {
+        const result = await getAllStates();
+        res.status(200).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getAllProductsByStatus = async (req, res, next) => {
+    const { limit, offset, } = pageParser(req?.query);
+    const { searchContent, } = searchParser(req?.query);
+
+    const filterOperator = queryOperators.LIKE;
+
+    const userId = req?.user?._id;
+    const statusId = req.params.statusId;
+
+    try {
+        const result = await productStatusService.getAllDate({
+            statusId, userId, offset, limit, searchContent, filterOperator,
+        });
+
+        res.status(RESPONSE_STATUS_CODE.OK).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getProductStatusById = async (req, res, next) => {
+    try {
+        const bookId = parseInt(req.params.id);
+        const userId = req?.user?._id;
+
+        const data = await productStatusService.getInfoFromProductStatus(bookId, userId);
+
+        const key = buildCacheKey(cacheKeys.PRODUCT_STATE_ID, req);
+        await cacheDataWithExpiration(key, data);
+
+        res.status(RESPONSE_STATUS_CODE.OK).json(data);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const createProductStatus = async (req, res, next) => {
+    try {
+        const userId = req.user._id;
+        const checkAccount = await getUserVerificationStatus(userId);
+        if (!checkAccount) {
+            res.status(RESPONSE_STATUS_CODE.UNAUTHORIZED).json(updateMessage(MESSAGES.ACCOUNT_IS_NOT_VERIFY).user);
+        }
+
+        await productStatusService.addingNewProductStatus(userId, req.body);
+
+        const key = buildCacheKey(cacheKeys.PRODUCT_STATE_ID, req);
+        await deleteCacheEntry(key);
+
+        res.status(RESPONSE_STATUS_CODE.CREATED).json(
+            updateMessage(MESSAGES.SUCCESSFULLY_ADDED_BOOK_IN_COLLECTION).user
+        );
+    } catch (err) {
+        next(err);
+    }
+};
---
+++ services/productStatusService.ts
@@ -0,0 +1,81 @@
+import { responseMapper, EMappedType, } from '../Helpers';
+
+import db from '../Model';
+const Op = db?.Sequelize?.Op;
+
+export const getAllDate = async ({ statusId, userId, offset, limit, filterOperator, searchContent, }) => {
+    const queryOperator = Op[filterOperator];
+    const hasSearchContent = !!searchContent;
+
+    const query = {
+        where: { statusId, userId, },
+        include: [
+            {
+                model: db.Product,
+                required: true,
+                attributes: ['id', 'productTitle', 'genre', 'isVerify', 'authorId'],
+                include: [
+                    {
+                        model: db.File,
+                        attributes: ['id', 'src', 'uniqueName'],
+                    },
+                    {
+                        model: db.Author,
+                        attributes: ['name', 'image', 'isVerify', 'genre'],
+                    }
+                ],
+                where: hasSearchContent ? {
+                    [Op.or]: [
+                        {
+                            productTitle: { [queryOperator]: searchContent, },
+                        },
+                        {
+                            genre: { [queryOperator]: searchContent, },
+                        }
+                    ],
+                } : {},
+            },
+            {
+                model: db.User as 'user',
+                required: true,
+                attributes: ['email', 'id'],
+            },
+            {
+                model: db.State,
+                required: true,
+                attributes: ['stateName'],
+            }
+        ],
+        attributes: ['id', 'statusId', 'isDelete'],
+        order: [['id', 'ASC']],
+        offset,
+        limit,
+        raw: true,
+        nest: true,
+    };
+
+    const result = await db.ProductStatus.findAndCountAll(query);
+
+    const mappedResponse = responseMapper(result, EMappedType.PRODUCT_STATE);
+
+    return mappedResponse;
+};
+
+export const getInfoFromProductStatus = async (productId, userId) => {
+    return await db.ProductStatus.findOne({
+        where: { productId, userId, isDelete: false, },
+        attributes: ['statusId'],
+    });
+};
+
+export const addingNewProductStatus = async (userId, { productId, state, }) => {
+    const existingBook = await db.ProductStatus.findOne({ where: { productId, userId, isDelete: false, }, });
+
+    if (existingBook) {
+        existingBook.statusId = state;
+        return await existingBook.save();
+    }
+
+    const result = (await db.ProductStatus.create({ userId, productId, statusId: state, }))?.dataValues;
+    return result;
+};
