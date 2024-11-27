c62b8c3d36bf87ad2928f1125cd9e31ca754599a
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-27 08:35:02 +0200

    feat: Implement and add ProductService and ProductController

---
+++ controller/productController.ts
@@ -0,0 +1,128 @@
+import { MESSAGES, ESendEvents, queryOperators, cacheKeys, RESPONSE_STATUS_CODE, } from '../constants';
+
+import { emitToSocketEvent, } from '../Events';
+
+import { buildCacheKey, pageParser, searchParser, } from '../Helpers';
+
+import * as productService from '../services/productService';
+import * as fileService from '../services/fileService';
+import {
+    cacheDataWithExpiration,
+    deleteCacheEntry,
+    deleteKeysWithPrefix,
+} from '../services/cacheService';
+
+import { updateMessage, } from '../util';
+import {
+    getUserVerificationStatus,
+} from '../services/getUserVerificationStatus';
+
+export const getAllProducts = async (req, res, next) => {
+    const { limit, offset, } = pageParser(req?.query);
+    const { searchContent, } = searchParser(req?.query);
+
+    const filterOperator = queryOperators.LIKE;
+
+    try {
+        const result = await productService.getAllData({ offset, limit, filterOperator, searchContent, });
+
+        const key = buildCacheKey(cacheKeys.ALL_PRODUCTS, req);
+        await cacheDataWithExpiration(key, result);
+
+        res.status(RESPONSE_STATUS_CODE.OK).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getProductById = async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        const result = await productService.getDataById(id);
+
+        const key = buildCacheKey(cacheKeys.PRODUCT_ID, req);
+        await cacheDataWithExpiration(key, result);
+
+        res.status(RESPONSE_STATUS_CODE.OK).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const createProduct = async (req, res, next) => {
+    try {
+        const userId = req.user._id;
+        const checkAccount = await getUserVerificationStatus(userId);
+        if (!checkAccount) {
+            res.status(RESPONSE_STATUS_CODE.UNAUTHORIZED).json(updateMessage(MESSAGES.ACCOUNT_IS_NOT_VERIFY).user);
+            return;
+        }
+
+        const result = await productService.create(req.body);
+
+        if (result.id) {
+            emitToSocketEvent(ESendEvents.NEW_PRODUCT_ADDED, result);
+        }
+
+        const requestRespond = result?.user ? result?.user : { productId: result.id, };
+        const statusCode = result?.statusCode ? result?.statusCode : RESPONSE_STATUS_CODE.CREATED;
+        res.status(statusCode).json(requestRespond);
+
+        await deleteKeysWithPrefix(cacheKeys.ALL_PRODUCTS);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const addedImageOnProduct = async (req, res, next) => {
+    try {
+        if (!req.files) {
+            res.status(RESPONSE_STATUS_CODE.BAD_REQUEST).json(updateMessage(MESSAGES.PLEASE_ADDED_FILE).user);
+            return;
+        }
+        const { deliverFile, } = req.files;
+        const fileData = await fileService.addingFile(deliverFile, req.body);
+
+        res.status(RESPONSE_STATUS_CODE.OK).json(fileData);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const removeImageOnProduct = async (req, res, next) => {
+    try {
+        const fileId = req.params.id;
+        const resultFromUnlink = await fileService.removeFile(fileId);
+        res.status(resultFromUnlink.statusCode).json(resultFromUnlink.user);
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+// TODO For Future
+export const updateProduct = async (req, res, next) => {
+    try {
+        const id = parseInt(req.params.id);
+        const result = await productService.update(id, req.body);
+
+        const key = buildCacheKey(cacheKeys.PRODUCT_ID, req);
+        await deleteCacheEntry(key);
+
+        res.status(RESPONSE_STATUS_CODE.OK).json(result);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const deleteProduct = async (req, res, next) => {
+    const id = parseInt(req.params.id);
+
+    try {
+        await productService.remove(id);
+        res.status(RESPONSE_STATUS_CODE.NO_CONTENT).end();
+    } catch (err) {
+        next();
+    }
+};
---
+++ services/productService.ts
@@ -0,0 +1,112 @@
+import { MESSAGES, } from '../constants';
+import { responseMapper, EMappedType, mappedSingleObject, } from '../Helpers';
+
+import db from '../Model';
+const Op = db?.Sequelize?.Op;
+
+import { updateMessage, } from '../util';
+
+const ATTRIBUTES = ['name', 'image', 'genre', 'isVerify'];
+
+export const getAllData = async ({ offset, limit, filterOperator, searchContent, }) => {
+    const queryOperator = Op[filterOperator];
+
+    const query = {
+        include: [{
+            model: db.Author,
+            required: false,
+            attributes: ATTRIBUTES,
+            // where: searchContent ? {
+            //     name: { [queryOperator]: searchContent, },
+            // } : {},
+        },
+        {
+            model: db.File,
+            attributes: ['id', 'src', 'uniqueName'],
+        }],
+        order: [['id', 'ASC']],
+        attributes: ['id', 'productTitle', 'genre', 'isVerify'],
+        offset,
+        limit,
+        raw: true,
+        nest: true,
+        where: {},
+    };
+
+
+    !!searchContent && (query.where = {
+        [Op.or]: [
+            {
+                productTitle: { [queryOperator]: searchContent, },
+            },
+            {
+                genre: { [queryOperator]: searchContent, },
+            }
+        ],
+    });
+
+    const result = await db.Product.findAndCountAll(query);
+
+    const mappedResponse = responseMapper(result, EMappedType.PRODUCT);
+
+    return mappedResponse;
+};
+
+export const getDataById = async (id) => {
+    const result = await db.Product.findByPk(id, {
+        include: [
+            {
+                model: db.Author,
+                attributes: ATTRIBUTES,
+                required: false,
+            },
+            {
+                model: db.File,
+                required: false,
+                attributes: ['id', 'src', 'uniqueName'],
+            }
+        ],
+        raw: true,
+        nest: true,
+    });
+
+    const mappedResponse = mappedSingleObject(result, EMappedType.PRODUCT);
+
+    return mappedResponse;
+};
+
+export const create = async ({ author, productTitle, genre, }) => {
+    const existingBook = (await db.Product.findOne({ where: { productTitle, }, }))?.dataValues;
+    if (existingBook) {
+        return updateMessage(MESSAGES.BOOK_ALREADY_EXIST, 403);
+    }
+
+    if (!existingBook) {
+        const isAuthor = (await db.Author.findOne({ where: { name: author, }, }))?.dataValues;
+
+        if (!isAuthor) {
+            const createAuthor = (await db.Author.create({ name: author, }))?.dataValues;
+            author = createAuthor.id;
+        }
+        isAuthor && (author = isAuthor.id);
+    }
+
+    const create = (await db.Product.create({ productTitle, authorId: author, genre, }))?.dataValues;
+    return create;
+};
+
+export const update = async (id, { author, productTitle, }) => {
+    const data: any = [] //await Book.findByPk(id);
+
+    // data.authorName = author; // To Do Adding editing author name
+    data.productTitle = productTitle;
+    const result = await data.save();
+    return result;
+};
+
+
+export const remove = async (id) => {
+    const data = []// await Book.findByPk(id);
+    return data
+    // return data.destroy(); // To Do adding isDelete of True
+};
