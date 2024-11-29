ef336e3db856de960cf2381794dc57d23a9000ef
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-11-29 21:33:58 +0200

    chore: Extract Auth Header name into environment file

--- middleware/_cors.ts
+++ middleware/_cors.ts
@@ -1,8 +1,12 @@
+import 'dotenv/config';
+
+const { HEADER_AUTHORIZATION_NAME, } = process.env;
+
 export default (whitelist) => (req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', `${whitelist.join()}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, book-id');
+    res.setHeader('Access-Control-Allow-Headers', `Content-Type, ${HEADER_AUTHORIZATION_NAME}`);
 
     next();
 };
