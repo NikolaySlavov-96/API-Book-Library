d1304dc238784bee6ddd8b97e81bb6e306c402ee
NikolaySlavov-96 <nikolay.slavov.96@gmail.com>
2024-12-01 17:44:25 +0200

    chore: Update types and organize SocketEvents

--- constants/_socketEvents.ts
+++ constants/_socketEvents.ts
@@ -1,10 +1,12 @@
 export enum _EReceiveEvents {
-    USER_CONNECT = 'user_connect',
+    SUPPORT_ACCEPT_USER = 'support_accept_user',
+    SUPPORT_ACTIVITY = 'support_activity',
     SUPPORT_CHAT_USER_JOIN = 'support_chat_join',
     SUPPORT_CHAT_USER_LEAVE = 'support_chat_leave',
-    USER_ACCEPT_JOIN_TO_ROOM = 'user_accept_join_to_room',
-    SUPPORT_ACCEPT_USER = 'support_accept_user',
     SUPPORT_MESSAGE = 'support_message',
+    SUPPORT_MESSAGE_STATUS = 'support_message_status',
+    USER_ACCEPT_JOIN_TO_ROOM = 'user_accept_join_to_room',
+    USER_CONNECT = 'user_connect',
 }
 
 export enum _ESendEvents {
@@ -14,7 +16,9 @@ export enum _ESendEvents {
     NOTIFY_ADMINS_OF_NEW_USER = 'notify_admins_of_new_user',
     NOTIFY_FOR_CREATE_ROOM = 'notify_for_created_room',
     SUPPORT_ACCEPT_USER_ACKNOWLEDGMENT = 'support_accept_user_acknowledgment',
+    SUPPORT_ACTIVITY = 'support_activity',
     SUPPORT_CHAT_USER_JOIN_ACKNOWLEDGMENT = 'support_chat_user_join_acknowledgment',
     SUPPORT_MESSAGE = 'support_message',
+    SUPPORT_MESSAGE_STATUS = 'support_message_status',
     USER_CONNECT = 'user_connect',
 }
