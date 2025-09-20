68342e69a772afdaf1e864efa39cae29349f4afc
Nikolay Slavov <nikolay.slavov.96@gmail.com>
2025-09-20 11:35:36 +0300

    Update cors middleware

--- middleware/_cors.ts
+++ middleware/_cors.ts
@@ -1,12 +1,8 @@
-import 'dotenv/config';
-
-const { HEADER_AUTHORIZATION_NAME, } = process.env;
-
 export default (whitelist) => (req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', `${whitelist.join()}`);
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'HEAD, OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', `Content-Type, ${HEADER_AUTHORIZATION_NAME}`);
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
     next();
 };
